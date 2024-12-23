a02001b149da018c0c6fbff63065f58ec4ddfe56
JarlPenguin <jarlpenguin@outlook.com>
2024-12-23 08:13:02 -0300

    Add vendor for user 8.0.0 OPPS27.91-176-11-16 20 release-keys
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

---
+++ vendor/bin/init.mmi.acdb.sh
@@ -0,0 +1,28 @@
+#!/vendor/bin/sh
+echo "acdb_path"
+typeset -l carrier=$(getprop ro.carrier)
+
+latam=\
+"timbr tefbr amxbr retbr attmx \
+tefmx openmx amxmx amxar tefar \
+perar retar amxcl entcl tefcl \
+womcl amxco tefco tigco amxpe \
+niipe tefpe amxla retla1st retla "
+
+if echo "${latam[@]}" | /vendor/bin/toybox_vendor grep -w "$carrier" &>/dev/null; then
+# use audio parameter specialized for latam market
+path=/vendor/etc/acdbdata_bl
+elif [ $carrier == "retid" ]; then
+# use audio parameter specialized for indonesia 
+path=/vendor/etc/acdbdata_id
+else
+# use default audio parameter
+path=/vendor/etc/acdbdata 
+fi
+
+index=0
+for file in $(ls $path/*.acdb); do
+    setprop persist.audio.calfile$index $file 
+    index=$((index+1))
+done
+
